3091d8392f277c634b248d201d2a21542ecba4a1
Ahmet Ayyildiz <84086596+AhmetAyyildiz-coder@users.noreply.github.com>
2024-07-21 22:54:36 +0300

    Add project files.

---
+++ Core/Logging/LoggingExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using Serilog;
+
+public static class LoggerExtensions
+{
+    public static void LogWarning(this ILogger logger, Exception ex, string message, [CallerMemberName] string callerName = "")
+    {
+        logger.Warning(ex, $"Method: {callerName} - {message}");
+    }
+    public static void LogDetailedException(this ILogger logger, Exception ex, string customMessage = "", object parameter = null,
+       [CallerMemberName] string memberName = "",
+       [CallerFilePath] string filePath = "",
+       [CallerLineNumber] int lineNumber = 0)
+    {
+        var logMessage = $"{customMessage} \n" +
+                         $"Exception: {ex.Message} \n" +
+                         $"Method: {memberName} \n" +
+                         $"File: {filePath} \n" +
+                         $"Line: {lineNumber} \n" +
+                         $"Parameter : {JsonConvert.SerializeObject(parameter)}";
+
+        logger.Warning(logMessage);
+    }
+
+    public static void LogDetailed(this ILogger logger, string customMessage = "", object parameter = null,
+      [CallerMemberName] string memberName = "",
+      [CallerFilePath] string filePath = "",
+      [CallerLineNumber] int lineNumber = 0)
+    {
+        var logMessage = $"{customMessage} \n" +
+                         $"Method: {memberName} \n" +
+                         $"File: {filePath} \n" +
+                         $"Line: {lineNumber} \n" +
+                         $"Parameter : {JsonConvert.SerializeObject(parameter)}";
+
+        logger.Warning(logMessage);
+    }
+}
